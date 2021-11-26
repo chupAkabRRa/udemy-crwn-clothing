40a45954af7f9cf6ec0fb6ebc665bb6d4521b27f
Dima Kostenich <dima.kostenich@gmail.com>
2021-11-27 00:02:32 +0300

    Homepage and SASS setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HomePage from "./homepage.component.jsx";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return <div className="App">
+      <HomePage />
+    </div>;
+  }
+}
+
+export default App;
